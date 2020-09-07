fdedf2a7cfc89648acafa5b9ed410184c61b584c
hundehausen <mail@grischadaum.de>
2020-09-07 22:05:07 +0200

    import fix

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const services = require("./services.js");
 const io = require("socket.io")(3000);
 
 let streamers = {};
+
+function onDisconnectOrTimeout(streamers) {
+  const disconnectedStreamer = Object.values(streamers).find(
+    (streamer) => streamer.streamerSocketId === socket.id
+  );
+  streamers[disconnectedStreamer.streamerName].online = false;
+  console.log(
+    "streamer: " +
+      streamers[disconnectedStreamer.streamerName].streamerName +
+      " disconnected"
+  );
+}
 
 function addStreamer(socketId, streamerInfo) {
   const existingStreamer = streamers[streamerInfo.streamerName];
