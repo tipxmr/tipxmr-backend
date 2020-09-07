088ee5486bd0fa3ab5906f6c6c978716a3cb87c6
hundehausen <mail@grischadaum.de>
2020-09-07 13:23:19 +0200

    add new streamer to array

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const io = require("socket.io")(3000);
 
+let streamer = [];
+
+function addStreamer(socketId, streamerInfo) {
+  const existingStreamer = streamer.find(
+    (streamer) => streamer.streamerName === streamerInfo.streamerName
+  );
+  // if Streamer existits, update streamer id, if socketId differs
+  if (existingStreamer !== undefined) {
+    if (existingStreamer.streamerSocketId !== socketId) {
+      existingStreamer.streamerSocketId = socketId;
+    }
+  } else {
+    // new streamer added
+    streamer.push({
+      streamerSocketId: socketId,
+      streamerName: streamerInfo.streamerName,
+      hashedSeed: streamerInfo.hashedSeed,
+    });
+  }
+}
+
 io.on("connect", (socket) => {
+  // Streamer connects
+  socket.on("streamerInfo", (streamerInfo) => {
+    addStreamer(socket.id, streamerInfo);
+    console.dir(streamer);
+  });
+  // donator requests
+  socket.on("getSubaddress", (data) => {
+    io.to(streamerSocketId);
+  });
   socket.on("returnSubaddress", (subaddress) => {
     console.log("Subaddress", subaddress);
   });
