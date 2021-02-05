a48c60f96787fddbd5915875cc290d8d29b93393
hundehausen <mail@grischadaum.de>
2021-02-05 23:06:22 +0100

    [TIPXMR-51] refactoring

--- src/data/streamerTestDB.ts
+++ src/data/streamerTestDB.ts
@@ -3,26 +3,9 @@ Alex: fa80ac5814a6fddee2fa29a1e62f5de4e3a233f07a51e886a3a1e7a8bce5abf7
 Grischa: b8185a25bbe3b4206e490558ab50b0567deca446d15282e92c5c66fde6693399
 Pronas: 
 */
-import {StreamerInterface as Streamer} from "./streamerInterface";
+import { StreamerInterface as Streamer } from "./streamerInterface";
 const alex: Streamer = {
-  _id:
-    "fa80ac5814a6fddee2fa29a1e62f5de4e3a233f07a51e886a3a1e7a8bce5abf7",
-  creationDate: new Date("2020-09-01"),
-  displayName: "AlexAnarcho",
-  isOnline: false,
-  restoreHeight: 674675,
-  streamerSocketId: "",
-  userName: "alexanarcho",
-  profilePicture:
-    "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=", // allow the user to upload a user avatar
-  isPremium: false,
-  stream: {
-    category: "politics",
-    description: "I am a great streamer.",
-    language: "🇩🇪",
-    platform: "twitch",
-    url: "https://www.twitch.tv/n00bprogrammer",
-  },
+  _id: "fa80ac5814a6fddee2fa29a1e62f5de4e3a233f07a51e886a3a1e7a8bce5abf7",
   animationSettings: {
     bgImg: "",
     charLimit: 99,
@@ -40,31 +23,28 @@ const alex: Streamer = {
     showGoal: true,
     sound: "",
   },
+  creationDate: new Date("2020-09-01"),
+  displayName: "AlexAnarcho",
   donationStats: {
     allDonations: [],
     largestDonation: 0,
     totalDonations: 0,
   },
+  stream: {
+    category: "politics",
+    description: "I am a great streamer.",
+    language: "🇩🇪",
+    platform: "twitch",
+    url: "https://www.twitch.tv/n00bprogrammer",
+  },
+  streamerSocketId: "",
+  userName: "alexanarcho",
+  profilePicture:
+    "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
 };
 
 const grischa: Streamer = {
-  _id:
-    "b8185a25bbe3b4206e490558ab50b0567deca446d15282e92c5c66fde6693399",
-  creationDate: new Date("2020-09-01"),
-  displayName: "hundehausen",
-  isOnline: false,
-  isPremium: false,
-  profilePicture: "",
-  restoreHeight: 667580,
-  streamerSocketId: "",
-  userName: "hundehausen",
-  stream: {
-    category: "XXX",
-    description: "I am a greater streamer.",
-    language: "🇩🇪",
-    platform: "Chaturbate",
-    url: "chaturbate.com",
-  },
+  _id: "b8185a25bbe3b4206e490558ab50b0567deca446d15282e92c5c66fde6693399",
   animationSettings: {
     bgImg: "",
     charLimit: 100,
@@ -82,11 +62,26 @@ const grischa: Streamer = {
     showGoal: false,
     sound: "",
   },
+  creationDate: new Date("2020-09-01"),
+  displayName: "hundehausen",
   donationStats: {
     allDonations: [],
     largestDonation: 0,
     totalDonations: 0,
   },
+  isOnline: false,
+  isPremium: false,
+  profilePicture: "",
+  restoreHeight: 667580,
+  stream: {
+    category: "XXX",
+    description: "I am a greater streamer.",
+    language: "🇩🇪",
+    platform: "Chaturbate",
+    url: "chaturbate.com",
+  },
+  streamerSocketId: "",
+  userName: "hundehausen",
 };
 
 const pronas: Streamer = {
@@ -110,12 +105,16 @@ const pronas: Streamer = {
   },
   creationDate: new Date("2020-09-15"),
   displayName: "Pronas",
-  isOnline: false, 
+  donationStats: {
+    allDonations: [],
+    largestDonation: 0,
+    totalDonations: 0,
+  },
+  isOnline: false,
   isPremium: true,
   profilePicture: "",
   restoreHeight: 667580,
   streamerSocketId: "",
-  userName: "pronas",
   stream: {
     category: "technology",
     description: "I am the greatest streamer.",
@@ -123,11 +122,7 @@ const pronas: Streamer = {
     platform: "youtube",
     url: "youtube.com",
   },
-  donationStats: {
-    allDonations: [],
-    largestDonation: 0,
-    totalDonations: 0,
-  },
+  userName: "pronas",
 };
 
 exports.alex = alex;
